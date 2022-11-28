dca1ddc49daee997094837431c6df37083a3730d
상혁 <>
2022-11-28 20:25:06 +0900

    add : MySQL DB 연동 테스트코드 작성

---
+++ src/test/java/com/tophyuk/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tophyuk.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/tophyuk/todo/repository/TodoRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.tophyuk.todo.repository;
+
+import com.tophyuk.todo.domain.Todo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TodoRepositoryTest {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    void save() {
+        Todo todo = new Todo();
+        todo.setContents("밥먹기");
+        todo.setComplete(Boolean.TRUE);
+        todo.setModifiedDate(LocalDate.now());
+        this.todoRepository.save(todo);
+    }
+
+}
