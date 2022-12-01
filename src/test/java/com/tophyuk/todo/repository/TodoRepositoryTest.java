43192f4eb93ebbf76bd7a478483f39d4f0608374
상혁 <>
2022-12-01 23:02:34 +0900

    feat : todo 삭제 테스트코드 추가

--- src/test/java/com/tophyuk/todo/repository/TodoRepositoryTest.java
+++ src/test/java/com/tophyuk/todo/repository/TodoRepositoryTest.java
@@ -1,13 +1,14 @@
 package com.tophyuk.todo.repository;
 
 import com.tophyuk.todo.domain.Todo;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class TodoRepositoryTest {
@@ -17,11 +18,31 @@ public class TodoRepositoryTest {
 
     @Test
     void create() {
+        //given
         Todo todo = new Todo();
         todo.setContents("밥먹기");
         todo.setComplete(Boolean.TRUE);
         todo.setModifiedDate(LocalDate.now());
-        this.todoRepository.save(todo);
+
+        //when
+        Todo saveTodo = this.todoRepository.save(todo);
+
+        //then
+        Assertions.assertThat(saveTodo.getContents()).isEqualTo(todo.getContents());
+    }
+
+    @Test
+    void delete() {
+        //given
+        Long id = Long.valueOf(2);
+        Todo findId = todoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+
+        //when
+        todoRepository.delete(findId);
+
+        //then
+        assertThatThrownBy(() -> todoRepository.findById(id)).isInstanceOf(IllegalArgumentException.class);
+
     }
 
 }
